0d749b1da830b235fc2ee7e2dab85b14452f5757
Bahaa Aldeen <Bahaa Aldeen@DESKTOP-5GJDND0>
2020-08-18 01:59:03 +0300

    Test

--- app/User.php
+++ app/User.php
@@ -9,7 +9,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     use Notifiable;
-
+    // First
     /**
      * The attributes that are mass assignable.
      *
@@ -33,7 +33,6 @@ class User extends Authenticatable
      * @var array
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
         'email_verified_at' => 'datetime',
     ];
 }
